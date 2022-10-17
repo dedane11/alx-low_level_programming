ac03bcffff1716f6e49ff7260dc2660b5f8690a1
dedane11 <meetdedane@gmail.com>
2022-10-17 15:41:11 -0700

    My bx commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
-
-
 /*
- *
- *
- *   * Description: prints "Programming is like building a multilingual puzzle"
- *
- *	@@
+ * i
+ * Description: prints "Programming is like building a multilingual puzzle"
  */
-
-int 4 - puts(void)
-
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
