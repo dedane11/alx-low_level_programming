58cdaee4429d58e1c66d4be93679732fe68d29af
dedane11 <meetdedane@gmail.com>
2022-10-17 16:10:10 -0700

    My bv commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * i
  * Description: prints "Programming is like building a multilingual puzzle"
  */
-int 4 - puts(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
