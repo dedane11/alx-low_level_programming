fe0605b66b0a36b515699dbf5282ab7336e47df0
dedane11 <meetdedane@gmail.com>
2022-10-18 07:10:18 -0700

    My bnm commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
- *
- *
- * Description: main print "Programming is like building a multilingual puzzle"
- *
- * Return: Always 0 (Success)
- * /
-int main(puts)
+/**
+* main - Entry point
+*
+* Description: main print "Programming is like building a multilingual puzzle"
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
