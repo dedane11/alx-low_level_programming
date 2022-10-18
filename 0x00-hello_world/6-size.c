717c1b86859de969a606118eee21d3de06c2efa4
dedane11 <meetdedane@gmail.com>
2022-10-18 08:36:05 -0700

    My bne commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,10 @@
 /**
 * main - print the size of various types
 *
-* Description: prints the size of various 
-* types on the computer it is compileid and run on
+* Description: prints the size of various
+* types on the computer it is compiled and run on
 *
+* Return: Always 0 (Success)
 */
 
 int main(void)
@@ -22,3 +23,4 @@ int main(void)
 
 	return (0);
 }
+
