b7de61e57ff7a299ef7b7602026fb3d63231b51e
dedane11 <meetdedane@gmail.com>
2022-10-20 12:50:29 -0700

    My brq commit

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,21 @@
+#include "mary.h"
+
+#include <stdio.h>
+
+
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+
+
+
+int main(void)
+
+{
+
+	print_numbers();
+
+	return (0);
+}
