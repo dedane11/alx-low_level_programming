e26becb4a003e6649d4571700d99d85050042e5f
dedane11 <meetdedane@gmail.com>
2022-10-21 05:27:31 -0700

    My brr commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include "allison.h"
+
+
+/**
+* main - prints Chukwudum
+*
+* Description: prints Chukwudum
+*
+* Return: 0 (Success)
+*/
+
+int main(void)
+
+{
+
+	char text[10] = "Chukwudum";
+
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+
+	{
+
+		_putchar(text[i]);
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
